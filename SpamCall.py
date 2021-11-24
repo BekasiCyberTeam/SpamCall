d9e306fb2ea1c06709a5b26e5bb9ef1fcf969ff9
Bekasi Cyber Team (BCT) <94917979+BekasiCyberTeam@users.noreply.github.com>
2021-11-24 09:53:21 +0700

    Add files via upload

---
+++ SpamCall.py
@@ -0,0 +1,24 @@
+# module
+import os,sys,time,requests
+from time import sleep
+# tampilan
+os,system("clear")
+sleep(1)
+os,system("figlet SpamCall")
+banner= """
+=====================================
+{ • } Author : MR.CyberHack
+{ • } Team : Bekasi Cyber Team
+====================================="""
+sleep(1)
+print(banner)
+nomor = input("Nomor Target: ")
+jumlah = int("input(Jumlah Spam: ")
+
+url = "https://id.jagreward.com/member/verify-mobile/"
+ua = {'Host': "id.jagreward.com",'Connection': "keep-alive",'User-Agent': 'Mozilla/5.0 (Linux; Android 8.1.0; vivo 1724) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36'}
+dat = {"method": "CALL","countryCode": "id",}
+
+for i in range(jumlah):
+send = requests.post(url+nomor, headers=ua, data=dat)
+print(" [â€¢] Status ~+> ",(send.json()["message"]))
